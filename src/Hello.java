59c44e3096c342d38ca0d4716434b6d3a76b5cc1
nqwrt <nqwrt@ymail.com>
2020-05-16 00:15:10 +0900

    두번째 커밋

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,31 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int n1 = 10000;
+		int n2 = 2000;
+		int n3 = 300;
+		
+		int max=0;	
+		
+		if(n1>n2){
+			
+			if(n1>n3){
+				max = n1;
+			}else{
+				max = n3;
+			}
+			
+		}else{
+			
+			if(n2>n3){
+				max = n2;
+			}else{
+				max = n3;
+			}			
+		}
+		
+		System.out.println("최대값은 " +  max);
 	}
 
 }
